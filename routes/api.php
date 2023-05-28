88018ac899c1eafda7befcfdc45114c7766fa928
amarizzal <rizal.ammar91@yahoo.com>
2023-05-28 14:06:06 +0700

    tambah fitur login dan logout

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,8 @@ use App\Http\Controllers\PostController;
 //     return $request->user();
 // });
 
-Route::get('/posts',[PostController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/posts',[PostController::class, 'index']);
 Route::get('/posts/{id}',[PostController::class, 'show']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/logout', [AuthenticationController::class, 'logout']);
