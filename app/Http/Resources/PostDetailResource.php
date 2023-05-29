7e949afec4b2dba6e7217ca788b01e6bfbd7ac48
amarizzal <rizal.ammar91@yahoo.com>
2023-05-29 09:49:52 +0700

    tambah fitur upload gambar

--- app/Http/Resources/PostDetailResource.php
+++ app/Http/Resources/PostDetailResource.php
@@ -17,6 +17,7 @@ class PostDetailResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
+            'image' => $this->image,
             'author' => $this->author,
             'writer' => $this->whenLoaded("writer"),
             'comments' => $this->whenLoaded("comments", function() {
